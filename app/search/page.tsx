a708fc578e1ae444499cb9a70fc56390668c5187
inasan1234 <ryota.ina.0701@gmail.com>
2025-05-10 00:15:14 +0900

    set-search-box

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,37 @@
+'use client';
+
+import {useState} from 'react';
+
 export default function SearchPage() {
-  return <div>開発中</div>;
+  // save the search query to state
+  // and set the initial value to an empty string
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Search query:', query);
+  };
+
+  return (
+    <div className="max-w-3xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-4">検索する</h1>
+
+      <form onSubmit={handleSubmit} className='flex'>
+        <input
+          type="text"
+          className="flex-1 border rounded-l px-4 py-2"
+          placeholder="キーワードを入力"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="bg-purple-600 text-white px-4 rounded-r hover:bg-purple-700"
+        >
+          検索
+        </button>
+      </form>
+    </div>
+  );
 }
 
